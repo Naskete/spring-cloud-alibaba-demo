54317894704676c7e31613367145e74951e2b7b5
Naskete <1776795871@qq.com>
2022-07-13 00:19:00 +0800

    test sentinel

--- shop-order/src/main/java/com/naskete/controller/OrderController3.java
+++ shop-order/src/main/java/com/naskete/controller/OrderController3.java
@@ -1,19 +1,25 @@
 package com.naskete.controller;
 
+import com.naskete.service.Imp.OrderServiceImpl3;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class OrderController3 {
+    @Autowired
+    OrderServiceImpl3 orderServiceImpl3;
     @GetMapping("/order/message1")
     public String message1() {
+        orderServiceImpl3.message();
         return "message1";
     }
 
     @GetMapping("/order/message2")
     public String message2() {
+        orderServiceImpl3.message();
         return "message2";
     }
 }
